6945687d47f5d39cd2cb5cc99c1d0c37ed60f56c
codenamemomi <akinrogundecodenamemomi@gmail.com>
2025-03-15 13:48:22 +0100

    Removed receipt directory from Git tracking

--- receipt/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from .views import ApprovedReceiptView, ReceiptView
-
-urlpatterns = [
-    path('receipt/', ReceiptView.as_view(), name='receipt'),
-    path('approve/', ApprovedReceiptView.as_view(), name='approve-receipt'),
-    path('approve/<int:id>/', ApprovedReceiptView.as_view(), name='approve-receipt'),
-]
